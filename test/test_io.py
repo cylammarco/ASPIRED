38c1f50f2c61e845aaf18cf401113b58cc329d16
cylammarco <cylammarco@gmail.com>
2020-01-08 00:26:55 +0000

    need to figure out how to test...

--- test/test_io.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
-
-def test_image_reduction():
-    science_frame = aspired.ImageReduction('examples/lhs6328.list')
-    science_frame.reduce()
