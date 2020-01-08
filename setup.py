368e74a53d599898788b1aeec461b8588aca2893
cylammarco <cylammarco@gmail.com>
2020-01-08 17:43:04 +0000

    testing test try #9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 install_requires=[
     'scipy', 'numpy>1.16', 'matplotlib', 'astropy', 'reproject==0.5', 'ccdproc', 'astroscrappy',
     'spectres @ git+https://github.com/cylammarco/SpectRes#egg=SpectRes', 'plotly',
-    'rascal @ git+https://github.com/jveitchmichaelis/rascal.git'
+    'rascal @ git+https://git@github.com/jveitchmichaelis/rascal.git#egg=rascal'
     ]
 
 __packagename__ = "aspired"
