4f78a13b0216d62b69c53f6d92d92203202cbd5c
cylammarco <cylammarco@gmail.com>
2021-01-22 12:26:19 +0200

    Added more tests.

--- test/test_extraction.py
+++ test/test_extraction.py
@@ -1,6 +1,6 @@
 import numpy as np
+from astropy.io import fits
 from aspired import spectral_reduction
-
 
 def test_spectral_extraction():
     # Prepare dummy data
@@ -41,3 +41,42 @@ def test_spectral_extraction():
     count = np.mean(dummy_twodspec.spectrum_list[0].count)
     assert np.round(count).astype('int') == 47, 'Extracted count is ' + str(
         count) + ' but it should be 19.'
+
+def test_user_supplied_trace():
+
+    # Load the image
+    lhs6328_fits = fits.open(
+        'test/test_data/v_e_20180810_12_1_0_0.fits.gz')[0]
+
+    spatial_mask = np.arange(50, 200)
+    spec_mask = np.arange(50, 1024)
+
+    #
+    # Loading a single pre-saved spectral trace.
+    #
+    lhs6328_extracted = fits.open(
+        'test/test_output/test_full_run_science_0.fits')
+    lhs6328_trace = lhs6328_extracted[1].data
+    lhs6328_trace_sigma = lhs6328_extracted[2].data
+
+    lhs6328_twodspec = spectral_reduction.TwoDSpec(lhs6328_fits,
+                                                spatial_mask=spatial_mask,
+                                                spec_mask=spec_mask,
+                                                readnoise=2.34)
+
+    # Adding the trace and the line spread function (sigma) to the TwoDSpec
+    # object
+    lhs6328_twodspec.add_trace(trace=lhs6328_trace,
+                            trace_sigma=lhs6328_trace_sigma)
+
+    lhs6328_twodspec.ap_extract(apwidth=15,
+                                optimal=True,
+                                skywidth=10,
+                                skydeg=1,
+                                display=True)
+
+    lhs6328_twodspec.save_fits(
+        output='count',
+        filename=
+        'test/test_output/user_supplied_trace_for_spectral_extraction',
+        overwrite=True)
--- test/test_wavecal.py
+++ test/test_wavecal.py
@@ -1,6 +1,8 @@
 import numpy as np
+from astropy.io import fits
 from aspired.wavelengthcalibration import WavelengthCalibration
 from aspired.spectrum1D import Spectrum1D
+from aspired import spectral_reduction
 
 
 def test_wavecal():
@@ -55,3 +57,51 @@ def test_wavecal():
     wavecal.save_csv(output='wavecal',
                      filename='test/test_output/test_wavecal',
                      overwrite=True)
+
+def test_user_supplied_poly_coeff():
+    # Load the image
+    lhs6328_fits = fits.open(
+        'test/test_data/v_e_20180810_12_1_0_0.fits.gz')[0]
+    spatial_mask = np.arange(50, 200)
+    spec_mask = np.arange(50, 1024)
+
+    #
+    # Loading two pre-saved spectral traces from a single FITS file.
+    #
+    lhs6328 = spectral_reduction.TwoDSpec(lhs6328_fits,
+                                        spatial_mask=spatial_mask,
+                                        spec_mask=spec_mask,
+                                        cosmicray=True,
+                                        readnoise=2.34)
+
+    # Trace the spectra
+    lhs6328.ap_trace(nspec=2, display=False)
+
+    # Extract the spectra
+    lhs6328.ap_extract(apwidth=10, optimal=True, skywidth=10, display=False)
+
+    # Calibrate the 1D spectra
+    lhs6328_onedspec = spectral_reduction.OneDSpec()
+    lhs6328_onedspec.from_twodspec(lhs6328, stype='science')
+
+    fit_coeff =np.array([
+            3.09833375e+03, 5.98842823e+00, -2.83963934e-03, 2.84842392e-06,
+            -1.03725267e-09
+        ])
+    fit_type = 'poly'
+
+    # Note that there are two science traces, so two polyfit coefficients have to
+    # be supplied by in a list
+    lhs6328_onedspec.add_fit_coeff(fit_coeff, fit_type, stype='science')
+    lhs6328_onedspec.apply_wavelength_calibration(stype='science')
+
+    # Inspect reduced spectrum
+    lhs6328_onedspec.inspect_reduced_spectrum(stype='science')
+
+    # Save as a FITS file
+    lhs6328_onedspec.save_fits(
+        output='wavecal+count',
+        filename=
+        'test/test_output/user_supplied_wavelength_polyfit_coefficients',
+        stype='science',
+        overwrite=True)
