60c2883b48b57fc2a1509ccf0336db5d96c5f435
cylammarco <cylammarco@gmail.com>
2022-11-30 22:14:12 +0200

    more tuning of the added noie to the test case

--- test/test_extraction.py
+++ test/test_extraction.py
@@ -33,7 +33,7 @@ def gaussian(pixels, central_pixel):
 
 
 # background noise
-bg_level = 0.5
+bg_level = 1.0
 
 # Prepare dummy data
 # total signal should be [2 * (2 + 5 + 10) + 20] - [1 * 7] = 47
@@ -113,7 +113,7 @@ def test_spectral_extraction():
 
     count = np.mean(dummy_twodspec.spectrum_list[0].count)
     assert np.isclose(count, 55, atol=1.0), (
-        "Extracted count is " + str(count) + " but it should be 54."
+        "Extracted count is " + str(count) + " but it should be 55."
     )
 
     dummy_twodspec.inspect_extracted_spectrum(
